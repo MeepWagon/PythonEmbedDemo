33c0de7815281b7d93caf8185f1383a07375fbc9
Owner <angelushein2004@gmail.com>
2023-01-21 12:21:28 -0600

    changed python scripts

--- scripts/test.py
+++ scripts/test.py
@@ -1,9 +1,2 @@
-import gspread
-
-service_account = gspread.service_account()
-sheet = service_account.open("Job Options")
-
-worksheet = sheet.worksheet("Sheet1")
-
 def example():
-    return str(worksheet.acell('A4').value)    
+    return "Worked!"
